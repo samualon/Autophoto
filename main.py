db2b44de0ca588e4356e5d8834f87ff135ff5571
samualon <samuelcoryn@gmail.com>
2022-02-28 21:28:48 +0100

    zwaar_master_deze_shit_werkt

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import os
 
 psApp = win32com.client.Dispatch("Photoshop.Application")
 
-psApp.Open(r"C:\Users\Samuel\OneDrive - KU Leuven\Persoonlijk\Coding Projects\Python\Photoshop automation\test.psd")
+psApp.Open(r"C:\Users\Samuel\OneDrive - KU Leuven\Persoonlijk\Coding Projects\Python\Photoshop-automation\test.psd")
 
 doc = psApp.Application.ActiveDocument
 
-layer_ = doc.ArtLayers["Facts"]
-text_of_layer = layer_facts.TextItem
-text_of_layer.contents = "This is an example of a new text."
+text_layer = doc.ArtLayers["custom"]
+text_of_layer = text_layer.TextItem
+text_of_layer.contents = "werkt!"
