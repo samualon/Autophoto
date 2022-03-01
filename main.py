f19b54b983e243798a362aa620f9531d16e70734
Samuel <samuelcoryn@gmail.com>
2022-03-01 14:35:56 +0100

    added_not_working_export_module

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import win32com.client
 import os
 
-
+#Dispatch
 psApp = win32com.client.Dispatch("Photoshop.Application")
-
 psApp.Open(r"C:\Users\Samuel\OneDrive - KU Leuven\Persoonlijk\Coding Projects\Python\Photoshop-automation\test.psd")
-
 doc = psApp.Application.ActiveDocument
 
+#Edit text
 text_layer = doc.ArtLayers["custom"]
 text_of_layer = text_layer.TextItem
 text_of_layer.contents = "werkt!"
+
+#Export to png
+options = win32com.client.Dispatch('Photoshop.ExportOptionsSaveForWeb')
+options.Format = 13   # PNG Format
+options.PNG8 = False  # Sets it to PNG-24 bit
+
+pngfile = "result.png"
+
+doc.Export(ExportIn=pngfile, ExportAs=2, Options=options)
+
+
+print("Done!")
